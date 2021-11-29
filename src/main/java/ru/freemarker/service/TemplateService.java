1c8545daea470e775e3637f0fbcd0c7677500169
Dmitriy L <DandLitvinov@sberbank.ru>
2021-11-29 16:44:50 +0300

    add <#if> freemarker example

--- src/main/java/ru/freemarker/service/TemplateService.java
+++ src/main/java/ru/freemarker/service/TemplateService.java
@@ -23,7 +23,13 @@ public class TemplateService {
     public void freeMarkerExampleTest() throws IOException, TemplateException {
         // модель данных
         Map<String, Object> root = new HashMap<>();
+        //треугольные скобки <>
         root.put("name", "Freemarker");
+        root.put("configFpName", "ИМЯ ЕСТЬ");
+
+        //todo dot нотации - не работает
+        //root.put("configFpName.second", "ДОТ НОТАЦИИ");
+
         // шаблон
         Template temp = cfg.getTemplate("test.ftl");
         // обработка шаблона и модели данных
@@ -37,11 +43,18 @@ public class TemplateService {
         Map<String, Object> root = new HashMap<>();
         // для ${name}
         root.put("name", "Fremarker");
+
         // для <#list persons
         List<List> persons = new ArrayList<>();
         persons.add(Arrays.asList("Alexander", "Petrov", 47));
         persons.add(Arrays.asList("Slava", "Petrov", 13));
         root.put("persons", persons);
+
+//        // для квадратных скобок
+//        List<List> persons2 = new ArrayList<>();
+//        persons.add(Arrays.asList("Alexander2", "Petrov2", 47));
+//        persons.add(Arrays.asList("Alexander3", "Petrov3", 47));
+//        root.put("configFpName.second",persons2);
 
         try {
             Template template = cfg.getTemplate("mtemplate.ftl");
--- src/main/resources/templates/test.ftl
+++ src/main/resources/templates/test.ftl
@@ -6,5 +6,14 @@
 </head>
 <body>
 <h2>Hello ${name}!</h2>
+<#--это темплейт найдет-->
+<h3> <#if configFpName??> Exist config_fp_name: '${configFpName}'</#if></h3>
+<#--можно в квадратных скобках-->
+<h3> [#if configFpName??] square siblings: ${configFpName} 2'[/#if]</h3>
+<#--это темплейт не найдет и выпилит из html-->
+<#if notConfigFpName??><h3>  Exist config_fp_name: '${notConfigFpName}'</h3></#if>
+
+<#--Через точки (дот нотации) //TODO разобраться почему не работает-->
+<#--<h3> [#if configFpName.second??]  conf_name: "[=configFpName.second]"[/#if]</h3>-->
 </body>
 </html>
